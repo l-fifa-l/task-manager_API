378da2ee9582bfbcae09a0c91ed6708b31622491
vivek <starktestic@gmail.com>
2020-10-01 23:48:33 +0530

    Initial commit

---
+++ src/db/mongoose.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.MONGODB_URL,{
+    useNewUrlParser: true,
+    useCreateIndex:true
+})
+
+const Task =  mongoose.model('tasks',{
+    description:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    completed:{
+        type:Boolean,
+        required:false,
+        default:false,
+    }
+})
+
+//"/home/vivek/mongodb/bin/mongod --dbpath=/home/vivek/mongodb-data"
